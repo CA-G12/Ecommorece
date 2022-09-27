3ffb21ac016750944fff5b2f3a74d5522d13134f
MohammedOmar123 <mohammedbalousha60@gmail.com>
2022-09-27 11:23:51 +0300

    handle filter category

--- client/src/Components/landingPage/card/Card.js
+++ client/src/Components/landingPage/card/Card.js
@@ -12,25 +12,25 @@ function Counter() {
   );
 }
 
-function Container() {
+function Container({ card }) {
   return (
     <>
-      <img className="product-img" alt="product-img" src="http://cdn.shopify.com/s/files/1/0601/6024/5917/products/the-heavyweight-t-shirt_white_1_53f1397f-5b41-429f-8569-cbc1c4c174aa.jpg?v=1661175550" />
-      <p className="product-name">T-shirt</p>
-      <h3 className="product-price">$49.50</h3>
-      <p className="product-category">white shirt</p>
+      <img className="product-img" alt="product-img" src={card.img_url} />
+      <p className="product-name">{card.name}</p>
+      <h3 className="product-price">{card.price}</h3>
+      <p className="product-category">{card.category}</p>
 
     </>
   );
 }
 
-function Card({ name }) {
+function Card({ name, card }) {
   if (name) {
     if (name === 'auth') {
       return (
         <div className="card">
 
-          <Container />
+          <Container card={card} />
           <Button className="add-btn" text="Add" widthh="3rem" />
         </div>
       );
@@ -43,16 +43,16 @@ function Card({ name }) {
           <div className="remove">
             <button type="submit">x</button>
           </div>
-          <Container />
+          <Container card={card} />
           <Counter />
 
         </div>
       );
     }
-    if (name === 'notAuth') {
+    if (name === 'home') {
       return (
         <div className="card">
-          <Container />
+          <Container card={card} />
         </div>
       );
     }
