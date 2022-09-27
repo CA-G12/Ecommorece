d02772bb9d25f87a90fb1ec0ac7f19b3c3a1cf4a
Bakeza <dbakeza2002@gmail.com>
2022-09-27 14:22:45 +0300

    fix eslint errors Relates #60

--- server/app.js
+++ server/app.js
@@ -1,24 +1,24 @@
-const { join } = require('path');
+const { join } = require('path')
 
-const express = require('express');
-const compression = require('compression');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require('express')
+const compression = require('compression')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
 // const router = ('../routers');
 
-const app = express();
+const app = express()
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3001)
 
-app.use(compression());
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+app.use(compression())
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
 
-app.use(express.static(join(__dirname, '..', '..', 'public')));
+app.use(express.static(join(__dirname, '..', '..', 'public')))
 app.get('/api', (req, res) => {
-  res.json({ message: 'home' }); // auth or home 
-});
+  res.json({ message: 'home' }) // auth or home
+})
 // app.use(router);
 
-module.exports = app;
+module.exports = app
