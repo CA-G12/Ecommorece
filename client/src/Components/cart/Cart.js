eb0b9837c4962a8466f7641a39295a8ed4371899
Bakeza <dbakeza2002@gmail.com>
2022-09-29 10:34:38 +0300

    adding buy button Relates #91

--- client/src/Components/cart/Cart.js
+++ client/src/Components/cart/Cart.js
@@ -1,11 +1,17 @@
-import {useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Nav from '../nav/Navbar'
 import Card from '../landingPage/card/Card'
+import Button from '../button/Button'
 import '../../style/cart.css'
 
 
+function reomoveAllItems() {
 
-function Cart({auth, setAuth}) {
+  return fetch(`/deleteAllItems`).catch(err => console.log(err))
+
+}
+
+function Cart({ auth, setAuth }) {
   const [cartData, setCardData] = useState([]);
 
   useEffect(() => {
@@ -14,15 +20,15 @@ function Cart({auth, setAuth}) {
 
   // console.log(cartData);
 
-  if(cartData){
+  if (cartData) {
     //  console.log(cartData, 'ddd', auth);
     return (
-    <>
-        <Nav  auth={auth} setAuth={setAuth} />
+      <>
+        <Nav auth={auth} setAuth={setAuth} />
         <div className='cartContainer'>
-         { cartData.map((cartItem) => <Card name={auth ? 'cart' : ''} card={cartItem} key={cartItem.cartId} /> )}
-          
-      </div>
+          {cartData.map((cartItem) => <Card name={auth ? 'cart' : ''} card={cartItem} key={cartItem.cartId} />)}
+          <Button onClick={()=>reomoveAllItems()} text='Delete' widthh='100px' />
+        </div>
 
       </>
     )
